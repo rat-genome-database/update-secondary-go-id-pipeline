da1153fa4212d60cc25e6b3d59d8f1ffd20b3ba2
tutajm <mtutaj@mcw.edu>
2025-06-20 14:08:04 -0500

    updated dependencies

--- src/main/dist/updateSecondaryGOIDs.sh
+++ src/main/dist/updateSecondaryGOIDs.sh
@@ -5,7 +5,7 @@ APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=mtutaj@mcw.edu
 if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST="rgd.devops@mcw.edu,jrsmith@mcw.edu"
+  EMAIL_LIST="rgd.devops@mcw.edu jrsmith@mcw.edu"
 fi
 
 $APPDIR/_run.sh "$@" 2>&1 > $APPDIR/run.log
